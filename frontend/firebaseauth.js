3f19d65d733075c8979ecc93ed0aacaf99d67af9
Manoj-Krishna-Chandragiri <manojkrishnachandragiri@gmail.com>
2025-01-24 19:44:55 +0530

    Added Fast Api

---
+++ frontend/firebaseauth.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD3gISvqNfQrSWGvyimHCuzbXtBonNomGU",
+    authDomain: "rail-madad-c32f8.firebaseapp.com",
+    projectId: "rail-madad-c32f8",
+    storageBucket: "rail-madad-c32f8.firebasestorage.app",
+    messagingSenderId: "979728331479",
+    appId: "1:979728331479:web:1ce26c7c8e8e089e043692"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
